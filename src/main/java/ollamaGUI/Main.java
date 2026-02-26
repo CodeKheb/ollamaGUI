008a0dbfa6036473776da9246404d3996544cba2
CodeKheb <kherbinbuenaventura@gmail.com>
2026-02-26 17:52:47 +0800

    Added CSS Styling and some formatting

--- src/main/java/ollamaGUI/Main.java
+++ src/main/java/ollamaGUI/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -25,9 +26,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
         responseArea = new TextArea();
         responseArea.setEditable(false);
         responseArea.setWrapText(true);
+        responseArea.setMaxWidth(Double.MAX_VALUE);
+        responseArea.setMaxHeight(Double.MAX_VALUE);
 
         userInput = new TextField();
         userInput.setPromptText("What's on your mind today?");
@@ -39,6 +43,7 @@ public class Main extends Application {
                userInput.clear();
                new Thread(() -> {
                    try {
+                       responseArea.clear();
                        LocalHost.OllamaParsedJson(prompt, responseArea);
                    }
                    catch (Exception err) {
@@ -52,8 +57,13 @@ public class Main extends Application {
 
 
         VBox root = new VBox(10, responseArea, userInput, sendPrompt);
+
+        VBox.setVgrow(responseArea, Priority.ALWAYS);
         root.setPadding(new Insets(10));
         Scene mainScene = new Scene(root, 800, 600);
+        mainScene.getStylesheets().add(
+                getClass().getResource("/style.css").toExternalForm()
+        );
         stage.setScene(mainScene);
         stage.setTitle("Ollama GUI using javaFX");
         stage.show();
